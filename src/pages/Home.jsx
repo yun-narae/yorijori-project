d1c98f37a8db60aacb525939e47ed4545ae19c3c
seulbinim <skfo0827@naver.com>
2025-06-22 21:02:16 +0900

    BaseButton 테스트 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import BaseButton from '../components/BaseButton/BaseButton';
 
 export default function Home() {
   return (
     <div>
         <h1 className="text-mo-title-lg text-[var(--color-gray-8)]">Home</h1>
+        <BaseButton text="수정하기" size="sm" iconName="plus"></BaseButton>
+        -
+        <BaseButton type="secondary" size="md" text="수정하기" iconName="plus"></BaseButton>
+        -
+        <BaseButton type="tertiary" state="disable" size="lg" text="수정하기" iconName="plus"></BaseButton>
     </div>
   );
 }
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import BaseButton from '../components/BaseButton/BaseButton';
 
 const Test = () => {
   const navigate = useNavigate();
@@ -9,12 +10,7 @@ const Test = () => {
       <h1 className="text-2xl text-[var(--color-gray-8)] font-bold mb-6">
         🌙 다크모드 유지 테스트 페이지입니다.
       </h1>
-      <button
-        onClick={() => navigate(-1)}
-        className="px-4 py-2 rounded-lg bg-[var(--color-secondary)] text-white hover:opacity-90 transition"
-      >
-        ← 뒤로가기
-      </button>
+      <BaseButton text="뒤로가기" iconName="arrow-left" size="md" onClick={() => navigate(-1)}></BaseButton>
     </div>
   );
 };
