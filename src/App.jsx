6ed29188743c39445efc06cd00346dd1a88a69c9
seulbinim <skfo0827@naver.com>
2025-07-11 14:04:38 +0900

    라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
 import Test from './pages/Test';
+import SignupPage from './pages/SignupPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/test" element={<Test />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/signup" element={<SignupPage />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,10 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import DarkModeToggle from './components/DarkModeToggle/DarkModeToggle';
 import Header from './components/Header/Header';
 
 export default function Layout() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <Header
@@ -12,7 +14,7 @@ export default function Layout() {
                 buttonTitle = "회원가입"
                 // Icon2Name = "delete"
                 onShowIcon2={() => console.log("메뉴")}
-                onButtonTitleClick={() => console.log("타이틀버튼")}
+                onButtonTitleClick={() => navigate("/signup")}
                 headerClass=""
                 fill
             />
