2b8c9df55552012092f10ffaf1bb304e7c6bb8f0
seulbinim <skfo0827@naver.com>
2025-07-27 11:08:58 +0900

    fix: getPbImageURL 환경 변수 적용 및 fallback 추가

--- src/lib/getPbImageURL.js
+++ src/lib/getPbImageURL.js
@@ -1,3 +1,5 @@
-export default function getPbImageURL(item,fileName = 'images'){
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item.images}`
+export default function getPbImageURL(item, fileName = 'images') {
+    const image = Array.isArray(item[fileName]) ? item[fileName][0] : item[fileName];
+    const baseURL = import.meta.env.VITE_PB_URL || "https://y-narae.pockethost.io";
+    return `${baseURL}/api/files/${item.collectionId || item.collectionName}/${item.id}/${image}`;
 }
