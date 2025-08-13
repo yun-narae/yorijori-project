c9b8df3e7b5a3df343f37c1ddd1f2a28fa5886fb
seulbinim <skfo0827@naver.com>
2025-08-13 21:04:45 +0900

    refactor: React.lazy와 Suspense를 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import { AuthProvider } from "./contexts/AuthContext";
-import Layout from './Layout';
-import Home from './pages/Home';
-import MyPage from './pages/MyPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import RegisterSuccess from './pages/RegisterSuccess';
-import FindPassword from "./pages/FindPassword";
-import PostCreate from "./pages/PostCreate";
+import Layout from "./Layout";
+
+const Home = lazy(() => import("./pages/Home"));
+const MyPage = lazy(() => import("./pages/MyPage"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const RegisterSuccess = lazy(() => import("./pages/RegisterSuccess"));
+const FindPassword = lazy(() => import("./pages/FindPassword"));
+const PostCreate = lazy(() => import("./pages/PostCreate"));
+
+// 각 페이지 스켈레톤
+import PostCreateSkeleton from "./components/Skeletons/PostCreateSkeleton";
 
 function App() {
     return (
@@ -15,14 +20,64 @@ function App() {
             <AuthProvider>
                 <Routes>
                     <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/myPage" element={<MyPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/register/success" element={<RegisterSuccess />} />
-                    <Route path="/login/find-password" element={<FindPassword />} />
-                    <Route path="/post/create" element={<PostCreate />} />
-                </Route>
+                        <Route
+                            path="/"
+                            element={
+                                <Suspense fallback={null}>
+                                    <Home />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/myPage"
+                            element={
+                                <Suspense fallback={null}>
+                                    <MyPage />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/login"
+                            element={
+                                <Suspense fallback={null}>
+                                    <Login />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/register"
+                            element={
+                                <Suspense fallback={null}>
+                                    <Register />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/register/success"
+                            element={
+                                <Suspense fallback={null}>
+                                    <RegisterSuccess />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/login/find-password"
+                            element={
+                                <Suspense fallback={null}>
+                                    <FindPassword />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="/post/create"
+                            element={
+                                // 이미 존재하는 PostCreateSkeleton 활용
+                                <Suspense fallback={<PostCreateSkeleton step={0} />}>
+                                    <PostCreate />
+                                </Suspense>
+                            }
+                        />
+                    </Route>
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
