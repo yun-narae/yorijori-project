64bde667ebf858f1349e0a7b997f65525b9fe7ef
narae <skfo0827@naver.com>
2025-09-28 09:52:41 +0900

    ci: React Query 도입 및 QueryClientProvider 적용 및 의존성

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./styles/tailwind.css";
 import App from "./App.jsx";
 import ConfirmProvider from "./components/Modal/ConfirmProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <ConfirmProvider>
-            <App />
-        </ConfirmProvider>
-    </StrictMode>,
+        <QueryClientProvider client={queryClient}>
+            <ConfirmProvider>
+                <App />
+            </ConfirmProvider>
+        </QueryClientProvider>
+    </StrictMode>
 )
