0facdba4c726ee9a735432452a201b3b94a86e66
narae <skfo0827@naver.com>
2025-10-20 13:58:38 +0900

    style: 로그인 실패 팝업창 확인버튼만 나오도록

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,6 +43,7 @@ const Login = () => {
                 title: "로그인 실패",
                 description: "이메일 또는 비밀번호를 확인해주세요.",
                 confirmText: "확인",
+                cancelText: ""
             });
         } finally {
             setSubmitting(false);
