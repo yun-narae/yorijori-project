4dd3e40de4c0544db97b74eab1eb354eda54d5de
narae <skfo0827@naver.com>
2025-10-23 11:40:23 +0900

    style: MainBanner 섹션 스타일링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,25 @@ import React from "react";
 import RecentPosts from "../components/HomeSections/RecentPosts";
 import PopularPosts from "../components/HomeSections/PopularPosts";
 import ParticipationPosts from '../components/HomeSections/ParticipationPosts';
+import MainBanner from '../components/Banner/MainBanner';
 
 export default function Home() {
     return (
-        <main
-            className="
+        <main>
+            {/* 메인 배너 */}
+            <MainBanner />
+            <div className="
                 flex flex-col gap-10
                 max-w-[1060px] mx-auto mt-6 desktop:mt-8 mb-8
                 px-3
-            "
-        >
-            {/* 인기 Top5 */}
-            <PopularPosts />
-            {/* 최근 등록된 모임 */}
-            <RecentPosts />
-            {/* 예약한 모임 */}
-            <ParticipationPosts />
+            ">
+                {/* 인기 Top3 */}
+                <PopularPosts />
+                {/* 최근 등록된 모임 */}
+                <RecentPosts />
+                {/* 예약한 모임 */}
+                <ParticipationPosts />
+            </div>
         </main>
     );
 }
